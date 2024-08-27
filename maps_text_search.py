77d201a659bfbd67d99f85f4994f213d0702f133
AndreaGasparMiramontes <142550697+AndreaGasparMiramontes@users.noreply.github.com>
2024-08-26 19:01:24 -0600

    Almost all current types working (Only places api)

--- maps_text_search.py
+++ maps_text_search.py
@@ -8,27 +8,28 @@ SCOPES = ['https://www.googleapis.com/auth/cloud-platform']
 
 service = create_service(client_secret_file, API_NAME, API_VERSION, SCOPES)
 def get_atractions(place,focus):
+    places_list = []
     query = "top attractions in " + place
     #CASO DE QUERER VER NATURALEZA
     if focus == 1:
         request_body = {
             "textQuery": query,
             "regionCode": 'MX',
-            "includedType" : "historical_landmark" #Solo puede ser un solo tipo, hasta ahora para naturaleza yo digo que park y historical_landmark
+            "includedType" : "national_park" 
         }
         response = service.places().searchText(
             body = request_body,
             fields = "places.id,places.displayName.text,places.types,places.rating,places.shortFormattedAddress,places.googleMapsUri,places.websiteUri"
         ).execute()
         try:
-            places_list = response['places']
+            places_list += response['places']
         except:
-            places_list = []
+            pass
 
         request_body = {
             "textQuery": query,
             "regionCode": 'MX',
-            "includedType" : "park"
+            "includedType" : "park" #Parque
         }
         response = service.places().searchText(
             body = request_body,
@@ -51,14 +52,14 @@ def get_atractions(place,focus):
             fields = "places.id,places.displayName.text,places.types,places.rating,places.shortFormattedAddress,places.googleMapsUri,places.websiteUri"
         ).execute()
         try:
-            places_list = response['places']
+            places_list += response['places']
         except:
-            places_list = []
+            pass
 
         request_body = {
             "textQuery": query,
             "regionCode": 'MX',
-            "includedType" : "museum"
+            "includedType" : "performing_arts_theater" #Teatro
         }
         response = service.places().searchText(
             body = request_body,
@@ -72,7 +73,7 @@ def get_atractions(place,focus):
         request_body = {
             "textQuery": query,
             "regionCode": 'MX',
-            "includedType" : "performing_arts_theater"
+            "includedType" : "church" #Iglesia
         }
         response = service.places().searchText(
             body = request_body,
@@ -83,7 +84,67 @@ def get_atractions(place,focus):
         except:
             pass
 
-    #CASO DE QUERER UN VIAJE BARATO
+    #CASO DE QUERER VER HISTORICO
+    elif focus == 3:
+        request_body = {
+            "textQuery": query,
+            "regionCode": 'MX',
+            "includedType" : "historical_landmark" 
+        }
+        response = service.places().searchText(
+            body = request_body,
+            fields = "places.id,places.displayName.text,places.types,places.rating,places.shortFormattedAddress,places.googleMapsUri,places.websiteUri"
+        ).execute()
+        try:
+            places_list += response['places']
+        except:
+            pass
+
+        request_body = {
+            "textQuery": query,
+            "regionCode": 'MX',
+            "includedType" : "museum" #Museo
+        }
+        response = service.places().searchText(
+            body = request_body,
+            fields = "places.id,places.displayName.text,places.types,places.rating,places.shortFormattedAddress,places.googleMapsUri,places.websiteUri"
+        ).execute()
+        try:
+            places_list += response['places']
+        except:
+            pass
+
+        request_body = {
+            "textQuery": query,
+            "regionCode": 'MX',
+            "includedType" : "tourist_attraction" #En este tipo generalmente entran edificios
+        }
+        response = service.places().searchText(
+            body = request_body,
+            fields = "places.id,places.displayName.text,places.types,places.rating,places.shortFormattedAddress,places.googleMapsUri,places.websiteUri"
+        ).execute()
+        try:
+            places_list += response['places']
+        except:
+            pass
+    
+    #CASO DE QUERER COMER BUENA COMIDA
+    elif focus == 4:
+        request_body = {
+            "textQuery": "Mejores lugares para comer en "+ place,
+            "regionCode": 'MX',
+            #"includedType" : "banquet_hall" 
+        }
+        response = service.places().searchText(
+            body = request_body,
+            fields = "places.id,places.displayName.text,places.types,places.rating,places.shortFormattedAddress,places.googleMapsUri,places.websiteUri"
+        ).execute()
+        try:
+            places_list += response['places']
+        except:
+            pass
+
+    #CASO DE QUERER UN VIAJE BARATO                   // Nota personal: no funciona así
     elif focus == 5:
         request_body = {
             "textQuery": query,
@@ -97,5 +158,14 @@ def get_atractions(place,focus):
         ).execute()
         places_list = response['places']
 
+    places_list = complete_list(places_list)
     df = pd.DataFrame(places_list)
     df.to_csv("places_results.csv", index = False)
+
+def complete_list(places_list):
+    new_places_list = []
+    for place in places_list:
+        place["horario"] = "Todo el dia :D"
+        if place not in new_places_list:
+            new_places_list.append(place)
+    return places_list
