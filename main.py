9c08a8e091179d9f079a6dd7631a20fe4b8b4481
AndreaGasparMiramontes <142550697+AndreaGasparMiramontes@users.noreply.github.com>
2024-09-22 20:55:23 -0600

    Entretainment added and final funtionality finished
    Added the entretainment focus and final functionality finished for one day and one focus

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ focus = "nada"
 pais = input("¿Where do you want to go?")
 estado = input()
 municipio = input()
-focus = int(input("What do you want to see?\n1)nature\n2)Culture\n3)History\n4)Gastronomic\n5)Bucket friendly"))
+focus = int(input("What do you want to see?\n1)nature\n2)Culture\n3)History\n4)Gastronomic\n5)Bucket friendly\n6)Entretainment"))
 weekday = input("Que dia de la semana planeas ir?(l,m,i,j,v,s,d)")
 maps.get_atractions(pais,estado,municipio,focus,weekday)
--- maps_text_search.py
+++ maps_text_search.py
@@ -20,17 +20,32 @@ weekdays = {'l':0,
             's':5,
             'd':6,}
 
+times = {'national_park' : 12,
+         'park' : 3,
+         'art_gallery' : 3,
+         'performing_arts_theater' : 4,
+         'church' : 2,
+         'historical_landmark' : 2,
+         'museum' : 4,
+         'tourist_attraction' : 2,
+         'cultural_center' : 2,
+         'zoo' : 6,
+         'aquarium' : 4,
+         'amusement_park' : 6
+}
+
 def get_atractions(pais, estado, municipio, focus,weekday):
     places_list = []
     query = f"top attractions in {municipio}, {estado}, {pais}"
 
     # Define the focus types mapping
     focus_dict = {
-        1: ["national_park", "park"],  # Naturaleza
-        2: ["art_gallery", "performing_arts_theater", "church", "museum"],  # Cultura
+        1: ["national_park", "park","aquarium","zoo"],  # Naturaleza
+        2: ["art_gallery", "performing_arts_theater", "church", "museum", "cultural_center"],  # Cultura
         3: ["historical_landmark", "museum", "tourist_attraction"],  # Historia
         4: [],  # Gastronomía (special handling)
-        5: []   # Viaje económico (special handling)
+        5: [],   # Viaje económico (special handling)
+        6: ["amusement_park","aquarium","zoo"]
     }
 
     if focus in focus_dict:
@@ -84,7 +99,8 @@ def get_atractions(pais, estado, municipio, focus,weekday):
 
     places_list = complete_list(places_list)
     graph = prepare_graph(places_list,weekdays[weekday])
-    run_pso(graph)
+    orden = run_pso(graph)
+    places_list = reorden(places_list,orden)
     df = pd.DataFrame(places_list)
     df.to_csv("places_results.csv", index=False)
 
@@ -96,6 +112,13 @@ def complete_list(places_list):
         place["reviews"] = place.get("reviews", [{}])[0].get("text", {}).get("text", "No reviews")
         place["latitude"] = place["location"]["latitude"]
         place["longitude"] = place["location"]["longitude"]
+        place["time"] = 0
+        for type in place["types"]:
+            try:
+                if times[type] > place["time"]:
+                    place["time"] = times[type]
+            except:
+                pass
         if place not in new_places_list:
             new_places_list.append(place)
     return new_places_list
@@ -104,23 +127,43 @@ def prepare_graph(places_list,weekday):
     nodos = []
     posible_nodes = []
     sorted_places = sorted(places_list, key=lambda place: place["ranking"])
+    time_spend = 0
+
+    place1 = sorted_places[0]
+    coordenada1 = dict()
+    coordenada1["x"] = (float(place1["location"]["latitude"]) + 180)*1000
+    coordenada1["y"] = (float(place1["location"]["longitude"]) + 180)*1000
+    coordenada1["name"] = place1["displayName"]
+    coordenada1["open_time"] = int(place1["hours"][weekday][-13]+place1["hours"][weekday][-12]+place1["hours"][weekday][-10]+place1["hours"][weekday][-9])
+    nodos.append(coordenada1)
+    sorted_places.remove(place1)
+
     for place in sorted_places:
         try:
-            if place["hours"][weekday][-6]!="C":
+            if place["hours"][weekday][-6]!="C" and time_spend < 12:
                 coordenada = dict()
                 coordenada["x"] = (float(place["location"]["latitude"]) + 180)*1000
                 coordenada["y"] = (float(place["location"]["longitude"]) + 180)*1000
                 coordenada["name"] = place["displayName"]
-                nodos.append(coordenada)
+                coordenada["open_time"] = int(place["hours"][weekday][-13]+place["hours"][weekday][-12]+place["hours"][weekday][-10]+place["hours"][weekday][-9])
+                if coordenada["open_time"] < nodos[0]["open_time"]:
+                    nodos.insert(0,coordenada)
+                else:
+                    nodos.append(coordenada)
+                time_spend = time_spend+place["time"]
         except:
             coordenada = dict()
             coordenada["x"] = (float(place["location"]["latitude"]) + 180)*1000
             coordenada["y"] = (float(place["location"]["longitude"]) + 180)*1000
             coordenada["name"] = place["displayName"]
+            coordenada["open_time"] = 25
             posible_nodes.append(coordenada)
+
+        print(nodos)
             
 
     amount = len(nodos)
+    print(amount)
     graph = Graph(amount_vertices=amount, starting_vertex=nodos[0]["name"])
     for i in range(amount):
         for j in range(i+1,amount):
@@ -141,4 +184,14 @@ def run_pso(graph):
     pso = PSO(graph, iterations=100, size_population=30, beta=2, alpha=2)
     pso.run()
     evolutions = pso.evolutions
-    print(evolutions[99])
+    return evolutions[99]
+
+def reorden(places_list,orden):
+    sorted_places_list = []
+    for i in orden:
+        for place in places_list:
+            if place["displayName"] == i:
+                sorted_places_list.append(place)
+    
+    return sorted_places_list
+
